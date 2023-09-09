fda43b5919a5d580b27ea543851335736c989edc
Christian Salazar <christian.salazar@pragma.com.co>
2023-09-09 09:44:11 -0500

    initial commit

---
+++ src/main/java/hackathon/representantes/RepresentantesApplication.java
@@ -0,0 +1,13 @@
+package hackathon.representantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepresentantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepresentantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hackathon/representantes/RepresentantesApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.representantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepresentantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
